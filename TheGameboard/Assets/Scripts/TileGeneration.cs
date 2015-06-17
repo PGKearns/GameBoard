9e2375a1d3b11df052aee6d975c92424a37143ef
PGKearns <kearnsgpeter@gmail.com>
2015-06-17 00:28:32 -0400

    I ahve been trying to add comments as i go, im not great at documentation but the script names should be descriptive of their purpose.

--- TheGameboard/Assets/Scripts/TileGeneration.cs
+++ TheGameboard/Assets/Scripts/TileGeneration.cs
@@ -10,14 +10,19 @@ public class TileGeneration : MonoBehaviour {
 	public GameObject brick;
 	private Button MyButton;
 
+	//this is the code that is run everytime that "Create Room Is pressed
 	public void ButtonClick () {
 		float offsetx = 0;
 		float offsety = 0;
+		//create the room object this will hold all of the tiles instatiated for the rest of the code
+		GameObject Room = new GameObject("Room");
+		Room.transform.parent = GameObject.Find("boardHolder").transform;
+		//create tiles one at a time
 		for (float y = starty; y < ysize.value; y++) {
 			for (float x =  startx; x < xsize.value; x++) {
 				//Instantiate(brick, new Vector3(x+offsetx, y+offsety, 0), Quaternion.identity);
 				GameObject tile = Instantiate(brick, new Vector3(x+offsetx, y+offsety, 0), Quaternion.identity) as GameObject; 
-				tile.transform.parent = GameObject.Find("boardHolder").transform;
+				tile.transform.parent = Room.transform;
 				offsetx=offsetx+0.1f;
 			}
 			offsety=offsety+0.1f;
