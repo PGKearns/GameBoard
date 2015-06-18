230cd8471b48d9de5d3fc7e98b39c606849a7eb0
PGKearns <kearnsgpeter@gmail.com>
2015-06-18 15:57:44 -0400

    Sean are you even there?

--- TheGameboard/Assets/Scripts/TileGeneration.cs
+++ TheGameboard/Assets/Scripts/TileGeneration.cs
@@ -7,6 +7,7 @@ public class TileGeneration : MonoBehaviour {
 	//public float starty;
 	public Slider xsize;
 	public Slider ysize;
+	public Slider radius;
 	public GameObject brick;
 	private Button MyButton;
 
@@ -31,7 +32,30 @@ public class TileGeneration : MonoBehaviour {
 		//startx +=  xsize.value;
 		//starty +=  ysize.value;
 	}
-	
+
+
+	public void CircleButtonClick () {
+		
+		//create the room object this will hold all of the tiles instatiated for the rest of the code
+		GameObject Room = new GameObject("Room");
+		Room.transform.parent = GameObject.Find("boardHolder").transform;
+		//create tiles one at a time
+		for (float y = 0; y < (ysize.value); y++) {
+			for (float x =  0; x < (xsize.value); x++) {
+				//Instantiate(brick, new Vector3(x+offsetx, y+offsety, 0), Quaternion.identity);
+				if ( x>(radius.value/2)){
+					GameObject tile = Instantiate(brick, new Vector3(x, y, 0), Quaternion.identity) as GameObject; 
+					tile.transform.parent = Room.transform;
+				}
+			}
+			
+			
+		}
+		//update the start position of the next room
+		//startx +=  xsize.value;
+		//starty +=  ysize.value;
+	}
+
 	// Update is called once per frame
 	void Update () {
 	//fuck update we dont need it.
