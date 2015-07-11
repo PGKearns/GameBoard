413e457d542e4b4178097e6995b64f6465796a38
PGKearns <kearnsgpeter@gmail.com>
2015-07-11 16:33:15 -0400

    Camera tracking

--- TheGameboard/Assets/Scripts/TileGeneration.cs
+++ TheGameboard/Assets/Scripts/TileGeneration.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class TileGeneration : MonoBehaviour {
 	//public float startx;
 	//public float starty;
+	public Camera cam;
 	public Slider xsize;
 	public Slider ysize;
 	public Slider radius;
@@ -13,13 +14,15 @@ public class TileGeneration : MonoBehaviour {
 
 	//this is the code that is run everytime that "Create Room Is pressed
 	public void ButtonClick () {
+		float camx = Mathf.Round(cam.transform.position.x);
+		float camy = Mathf.Round(cam.transform.position.y);
 
 		//create the room object this will hold all of the tiles instatiated for the rest of the code
 		GameObject Room = new GameObject("Room");
 		Room.transform.parent = GameObject.Find("boardHolder").transform;
 		//create tiles one at a time
-		for (float y = 0; y < (ysize.value); y++) {
-			for (float x =  0; x < (xsize.value); x++) {
+		for (float y = camy; y < (ysize.value+camy); y++) {
+			for (float x =  camx; x < (xsize.value+camx); x++) {
 				//Instantiate(brick, new Vector3(x+offsetx, y+offsety, 0), Quaternion.identity);
 				GameObject tile = Instantiate(brick, new Vector3(x, y, 0), Quaternion.identity) as GameObject; 
 				tile.transform.parent = Room.transform;
